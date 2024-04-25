07fe58fb38833a34cf4406c584d936287d4877d0
susankimble <babybluemac@comcast.net>
2024-04-25 10:52:36 -0400

    made add and remove changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 //console.log (cat);
 button.addEventListener("click", function (){
-    cat.classList.add("show");
+    if (cat.classList.contains("show")){
+        //console.log("yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back";
+        button.classList.add("disappear");
+
+    } else {
+        //console.log("no")
+        cat.classList.add("show");
+        button.innerText = "shoo, cat";
+        button.classList.remove("disappear");
+    }
 });
 
